20074dac4de57e6c69feb2fcaff77d49c33564ed
Luan Vinicius <luanvmc.000@gmail.com>
2024-06-23 23:03:46 -0300

    atualização quase completa

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -1,63 +1,353 @@
-// Função para carregar os cards do localStorage
-function loadCards() {
-    const cardContainer = document.getElementById('cardContainer');
-    cardContainer.innerHTML = '';
-    const cards = JSON.parse(localStorage.getItem('cards')) || [];
-    cards.forEach((card, index) => {
-        createCard(card.name, card.siteLink, card.imageLink, index);
+      // Função para carregar os cards do localStorage
+      function loadCards() {
+        const cardContainer = document.getElementById('cardContainer');
+        cardContainer.innerHTML = '';
+        const cards = JSON.parse(localStorage.getItem('cards')) || [];
+        cards.forEach((card, index) => {
+            createCard(card.name, card.siteLink, card.imageLink, index);
+        });
+    }
+
+    // Função para criar um card
+    function createCard(name, siteLink, imageLink, index) {
+        const cardContainer = document.getElementById('cardContainer');
+        const card = document.createElement('div');
+        card.className = 'col-sm-12 col-md-6 col-lg-4 d-flex align-items-stretch mb-4'; // Adiciona a classe mb-4 para margem inferior
+        card.innerHTML = `
+            <div class="card position-relative w-100">
+                <img src="${imageLink}" class="card-img-top" alt="Image">
+                <button class="delete-btn" onclick="deleteCard(${index})">&times;</button>
+                <button class="edit-btn" onclick="editCard(${index})"><i class="fas fa-edit"></i></button>
+                <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                    <h5 class="card-title text-center">${name}</h5>
+                    
+
+                    <a class="cta btn" href="${siteLink}" target="_blank">
+    <span>Acesse agora</span>
+    <span>
+      <svg width="66px" height="43px" viewBox="0 0 66 43" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g id="arrow" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <path class="one" d="M40.1543933,3.89485454 L43.9763149,0.139296592 C44.1708311,-0.0518420739 44.4826329,-0.0518571125 44.6771675,0.139262789 L65.6916134,20.7848311 C66.0855801,21.1718824 66.0911863,21.8050225 65.704135,22.1989893 C65.7000188,22.2031791 65.6958657,22.2073326 65.6916762,22.2114492 L44.677098,42.8607841 C44.4825957,43.0519059 44.1708242,43.0519358 43.9762853,42.8608513 L40.1545186,39.1069479 C39.9575152,38.9134427 39.9546793,38.5968729 40.1481845,38.3998695 C40.1502893,38.3977268 40.1524132,38.395603 40.1545562,38.3934985 L56.9937789,21.8567812 C57.1908028,21.6632968 57.193672,21.3467273 57.0001876,21.1497035 C56.9980647,21.1475418 56.9959223,21.1453995 56.9937605,21.1432767 L40.1545208,4.60825197 C39.9574869,4.41477773 39.9546013,4.09820839 40.1480756,3.90117456 C40.1501626,3.89904911 40.1522686,3.89694235 40.1543933,3.89485454 Z" fill="#FFFFFF"></path>
+          <path class="two" d="M20.1543933,3.89485454 L23.9763149,0.139296592 C24.1708311,-0.0518420739 24.4826329,-0.0518571125 24.6771675,0.139262789 L45.6916134,20.7848311 C46.0855801,21.1718824 46.0911863,21.8050225 45.704135,22.1989893 C45.7000188,22.2031791 45.6958657,22.2073326 45.6916762,22.2114492 L24.677098,42.8607841 C24.4825957,43.0519059 24.1708242,43.0519358 23.9762853,42.8608513 L20.1545186,39.1069479 C19.9575152,38.9134427 19.9546793,38.5968729 20.1481845,38.3998695 C20.1502893,38.3977268 20.1524132,38.395603 20.1545562,38.3934985 L36.9937789,21.8567812 C37.1908028,21.6632968 37.193672,21.3467273 37.0001876,21.1497035 C36.9980647,21.1475418 36.9959223,21.1453995 36.9937605,21.1432767 L20.1545208,4.60825197 C19.9574869,4.41477773 19.9546013,4.09820839 20.1480756,3.90117456 C20.1501626,3.89904911 20.1522686,3.89694235 20.1543933,3.89485454 Z" fill="#FFFFFF"></path>
+          <path class="three" d="M0.154393339,3.89485454 L3.97631488,0.139296592 C4.17083111,-0.0518420739 4.48263286,-0.0518571125 4.67716753,0.139262789 L25.6916134,20.7848311 C26.0855801,21.1718824 26.0911863,21.8050225 25.704135,22.1989893 C25.7000188,22.2031791 25.6958657,22.2073326 25.6916762,22.2114492 L4.67709797,42.8607841 C4.48259567,43.0519059 4.17082418,43.0519358 3.97628526,42.8608513 L0.154518591,39.1069479 C-0.0424848215,38.9134427 -0.0453206733,38.5968729 0.148184538,38.3998695 C0.150289256,38.3977268 0.152413239,38.395603 0.154556228,38.3934985 L16.9937789,21.8567812 C17.1908028,21.6632968 17.193672,21.3467273 17.0001876,21.1497035 C16.9980647,21.1475418 16.9959223,21.1453995 16.9937605,21.1432767 L0.15452076,4.60825197 C-0.0425130651,4.41477773 -0.0453986756,4.09820839 0.148075568,3.90117456 C0.150162624,3.89904911 0.152268631,3.89694235 0.154393339,3.89485454 Z" fill="#FFFFFF"></path>
+        </g>
+      </svg>
+    </span> 
+  </a>
+                </div>
+            </div>
+        `;
+        cardContainer.appendChild(card);
+    }
+
+    // Função para excluir um card
+    function deleteCard(index) {
+        let cards = JSON.parse(localStorage.getItem('cards')) || [];
+        cards.splice(index, 1);
+        localStorage.setItem('cards', JSON.stringify(cards));
+        loadCards();
+    }
+
+    // Função para editar um card
+    function editCard(index) {
+        const cards = JSON.parse(localStorage.getItem('cards')) || [];
+        const card = cards[index];
+
+        // Preencher o formulário com os dados do card
+        document.getElementById('cardName').value = card.name;
+        document.getElementById('siteLink').value = card.siteLink;
+        document.getElementById('imageLink').value = card.imageLink;
+
+        // Exibir o formulário e os botões de cancelar e salvar edição
+        document.getElementById('formRow').style.display = 'block';
+        document.getElementById('showFormButton').style.display = 'none';
+        document.getElementById('cancelEditButton').style.display = 'block';
+        document.getElementById('saveChangesButton').style.display = 'block';
+
+        // Ocultar o botão de adicionar ao organizador
+        document.getElementById('addButton').style.display = 'none';
+
+        // Desabilitar outros botões de edição
+        const editButtons = document.querySelectorAll('.edit-btn');
+        editButtons.forEach(button => {
+            button.disabled = true;
+        });
+
+        // Adicionar o índice do card ao botão de salvar mudanças
+        document.getElementById('saveChangesButton').setAttribute('data-index', index);
+
+        // Scroll para o topo da página
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+
+    // Função para salvar as mudanças
+    function saveChanges() {
+        const index = document.getElementById('saveChangesButton').getAttribute('data-index');
+        const name = document.getElementById('cardName').value;
+        const siteLink = document.getElementById('siteLink').value;
+        const imageLink = document.getElementById('imageLink').value;
+
+        // Atualizar o card no localStorage
+        let cards = JSON.parse(localStorage.getItem('cards')) || [];
+        cards[index] = { name, siteLink, imageLink };
+        localStorage.setItem('cards', JSON.stringify(cards));
+
+        // Recarregar os cards
+        loadCards();
+
+        // Limpar o formulário e esconder os botões de cancelar e salvar edição
+        document.getElementById('cardForm').reset();
+        document.getElementById('formRow').style.display = 'none';
+        document.getElementById('showFormButton').style.display = 'block';
+        document.getElementById('cancelEditButton').style.display = 'none';
+        document.getElementById('saveChangesButton').style.display = 'none';
+
+        // Mostrar o botão de adicionar ao organizador novamente
+        document.getElementById('addButton').style.display = 'block';
+
+        // Reativar botões de edição
+        const editButtons = document.querySelectorAll('.edit-btn');
+        editButtons.forEach(button => {
+            button.disabled = false;
+        });
+    }
+
+    // Função para cancelar a edição
+    function cancelEdit() {
+        // Limpar o formulário e esconder os botões de cancelar e salvar edição
+        document.getElementById('cardForm').reset();
+        document.getElementById('formRow').style.display = 'none';
+        document.getElementById('showFormButton').style.display = 'block';
+        document.getElementById('cancelEditButton').style.display = 'none';
+        document.getElementById('saveChangesButton').style.display = 'none';
+
+        // Mostrar o botão de adicionar ao organizador novamente
+        document.getElementById('addButton').style.display = 'block';
+
+        // Reativar botões de edição
+        const editButtons = document.querySelectorAll('.edit-btn');
+        editButtons.forEach(button => {
+            button.disabled = false;
+        });
+    }
+
+    // Carregar os cards quando a página for carregada
+    window.onload = loadCards;
+
+    document.getElementById('cardForm').addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const name = document.getElementById('cardName').value;
+        const siteLink = document.getElementById('siteLink').value;
+        const imageLink = document.getElementById('imageLink').value;
+
+        const cards = JSON.parse(localStorage.getItem('cards')) || [];
+        cards.push({ name, siteLink, imageLink });
+        localStorage.setItem('cards', JSON.stringify(cards));
+
+        createCard(name, siteLink, imageLink, cards.length - 1);
+
+        // Limpar o formulário
+        document.getElementById('cardForm').reset();
+        document.getElementById('formRow').style.display = 'none';
+        document.getElementById('showFormButton').style.display = 'block';
+    });
+
+    // Mostrar o formulário quando o botão "Adicionar Card" for clicado
+    document.getElementById('showFormButton').addEventListener('click', function() {
+        document.getElementById('formRow').style.display = 'block';
+        document.getElementById('showFormButton').style.display = 'none';
+    });
+
+    // ------------------------------
+    // Função para fechar o formulário
+function closeForm() {
+    document.getElementById('formRow').style.display = 'none';
+    document.getElementById('showFormButton').style.display = 'block';
+    document.getElementById('cardForm').reset();
+}
+
+
+// verificar depois os códigos -------------------------------------------------------------------------
+
+
+
+// Função para reativar os botões de edição
+function enableEditButtons() {
+    const editButtons = document.querySelectorAll('.edit-btn');
+    editButtons.forEach(button => {
+        button.disabled = false;
     });
 }
 
-// Função para criar um card
-function createCard(name, siteLink, imageLink, index) {
-    const cardContainer = document.getElementById('cardContainer');
-    const card = document.createElement('div');
-    card.className = 'col-sm-12 col-md-6 col-lg-4 d-flex align-items-stretch mb-4'; // Adiciona a classe mb-4 para margem inferior
-    card.innerHTML = `
-        <div class="card position-relative w-100">
-            <button class="delete-btn" onclick="deleteCard(${index})">&times;</button>
-            <img src="${imageLink}" class="card-img-top" alt="Image">
-            <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                <h5 class="card-title text-center">${name}</h5>
-                <a href="${siteLink}" target="_blank" class="btn btn-primary btn-block mt-3">Visitar Site</a>
-            </div>
-        </div>
-    `;
-    cardContainer.appendChild(card);
+// Função para editar um card
+function editCard(index) {
+    const cards = JSON.parse(localStorage.getItem('cards')) || [];
+    const card = cards[index];
+
+    // Preencher o formulário com os dados do card
+    document.getElementById('cardName').value = card.name;
+    document.getElementById('siteLink').value = card.siteLink;
+    document.getElementById('imageLink').value = card.imageLink;
+
+    // Exibir o formulário e os botões de cancelar e salvar edição
+    document.getElementById('formRow').style.display = 'block';
+    document.getElementById('showFormButton').style.display = 'none';
+    document.getElementById('cancelEditButton').style.display = 'block';
+    document.getElementById('saveChangesButton').style.display = 'block';
+
+    // Ocultar o botão de adicionar ao organizador
+    document.getElementById('addButton').style.display = 'none';
+
+    // Desabilitar outros botões de edição
+    enableEditButtons(); // Ativar os botões de edição antes de desativar o atual
+    const editButton = document.querySelectorAll('.edit-btn')[index];
+    editButton.disabled = true;
+
+    // Adicionar o índice do card ao botão de salvar mudanças
+    document.getElementById('saveChangesButton').setAttribute('data-index', index);
+
+    // Scroll para o topo da página
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
-// Função para excluir um card
-function deleteCard(index) {
-    let cards = JSON.parse(localStorage.getItem('cards')) || [];
-    cards.splice(index, 1);
-    localStorage.setItem('cards', JSON.stringify(cards));
-    loadCards();
-}
-
-// Carregar os cards quando a página for carregada
-window.onload = loadCards;
-
-document.getElementById('cardForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-
+// Função para salvar as mudanças
+function saveChanges() {
+    const index = document.getElementById('saveChangesButton').getAttribute('data-index');
     const name = document.getElementById('cardName').value;
     const siteLink = document.getElementById('siteLink').value;
     const imageLink = document.getElementById('imageLink').value;
 
-    const cards = JSON.parse(localStorage.getItem('cards')) || [];
-    cards.push({ name, siteLink, imageLink });
+    // Atualizar o card no localStorage
+    let cards = JSON.parse(localStorage.getItem('cards')) || [];
+    cards[index] = { name, siteLink, imageLink };
     localStorage.setItem('cards', JSON.stringify(cards));
 
-    createCard(name, siteLink, imageLink, cards.length - 1);
+    // Recarregar os cards
+    loadCards();
 
-    // Limpar o formulário
+    // Limpar o formulário e esconder os botões de cancelar e salvar edição
     document.getElementById('cardForm').reset();
     document.getElementById('formRow').style.display = 'none';
     document.getElementById('showFormButton').style.display = 'block';
-});
+    document.getElementById('cancelEditButton').style.display = 'none';
+    document.getElementById('saveChangesButton').style.display = 'none';
 
-// Mostrar o formulário quando o botão "Adicionar Card" for clicado
-document.getElementById('showFormButton').addEventListener('click', function() {
+    // Mostrar o botão de adicionar ao organizador novamente
+    document.getElementById('addButton').style.display = 'block';
+
+    // Reativar botões de edição
+    enableEditButtons();
+}
+
+// Função para cancelar a edição
+function cancelEdit() {
+    // Limpar o formulário e esconder os botões de cancelar e salvar edição
+    document.getElementById('cardForm').reset();
+    document.getElementById('formRow').style.display = 'none';
+    document.getElementById('showFormButton').style.display = 'block';
+    document.getElementById('cancelEditButton').style.display = 'none';
+    document.getElementById('saveChangesButton').style.display = 'none';
+
+    // Mostrar o botão de adicionar ao organizador novamente
+    document.getElementById('addButton').style.display = 'block';
+
+    // Reativar botões de edição
+    enableEditButtons();
+}
+
+
+
+
+
+// Função para editar um card
+function editCard(index) {
+    const cards = JSON.parse(localStorage.getItem('cards')) || [];
+    const card = cards[index];
+
+    // Preencher o formulário com os dados do card
+    document.getElementById('cardName').value = card.name;
+    document.getElementById('siteLink').value = card.siteLink;
+    document.getElementById('imageLink').value = card.imageLink;
+
+    // Exibir o formulário e os botões de cancelar e salvar edição
     document.getElementById('formRow').style.display = 'block';
     document.getElementById('showFormButton').style.display = 'none';
+    document.getElementById('cancelEditButton').style.display = 'block';
+    document.getElementById('saveChangesButton').style.display = 'block';
+
+    // Ocultar o botão de adicionar ao organizador
+    document.getElementById('addButton').style.display = 'none';
+
+    // Desabilitar outros botões de edição
+    const editButtons = document.querySelectorAll('.edit-btn');
+    editButtons.forEach(button => {
+        button.disabled = true;
+    });
+
+    // Adicionar o índice do card ao botão de salvar mudanças
+    document.getElementById('saveChangesButton').setAttribute('data-index', index);
+
+    // Scroll para o topo da página
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+// Função para salvar as mudanças
+function saveChanges() {
+    const index = document.getElementById('saveChangesButton').getAttribute('data-index');
+    const name = document.getElementById('cardName').value;
+    const siteLink = document.getElementById('siteLink').value;
+    const imageLink = document.getElementById('imageLink').value;
+
+    // Atualizar o card no localStorage
+    let cards = JSON.parse(localStorage.getItem('cards')) || [];
+    cards[index] = { name, siteLink, imageLink };
+    localStorage.setItem('cards', JSON.stringify(cards));
+
+    // Recarregar os cards
+    loadCards();
+
+    // Limpar o formulário e esconder os botões de cancelar e salvar edição
+    document.getElementById('cardForm').reset();
+    document.getElementById('formRow').style.display = 'none';
+    document.getElementById('showFormButton').style.display = 'block';
+    document.getElementById('cancelEditButton').style.display = 'none';
+    document.getElementById('saveChangesButton').style.display = 'none';
+
+    // Mostrar o botão de adicionar ao organizador novamente
+    document.getElementById('addButton').style.display = 'block';
+
+    // Reativar botões de edição
+    const editButtons = document.querySelectorAll('.edit-btn');
+    editButtons.forEach(button => {
+        button.disabled = false;
+    });
+}
+
+
+// --------------
+// Função para copiar o link de compartilhamento
+document.getElementById('copyLinkButton').addEventListener('click', function() {
+    const shareLink = window.location.href; // Obtém o link atual da página
+    navigator.clipboard.writeText(shareLink)
+        .then(() => {
+            alert('Link copiado com sucesso!');
+        })
+        .catch(err => {
+            console.error('Erro ao copiar link: ', err);
+            alert('Não foi possível copiar o link. Por favor, copie manualmente.');
+        });
+});
+
+// Função para mostrar o campo de inserir o link para acessar os cards de outra pessoa
+document.getElementById('openLinkFieldButton').addEventListener('click', function() {
+    document.getElementById('linkFieldContainer').style.display = 'block';
+});
+
+// Evento para redirecionar para o link inserido no campo
+document.getElementById('linkField').addEventListener('change', function() {
+    const link = document.getElementById('linkField').value.trim();
+    if (link !== '') {
+        window.location.href = link;
+    }
 });
